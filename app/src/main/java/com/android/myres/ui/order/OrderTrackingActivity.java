7babd3b683939d74d6201344ab654359e859db9f
tcqq <usertcqq@gmail.com>
2024-05-16 03:52:12 +0800

    add order tracking page

--- app/src/main/java/com/android/myres/ui/order/OrderTrackingActivity.java
+++ app/src/main/java/com/android/myres/ui/order/OrderTrackingActivity.java
@@ -1,6 +1,7 @@
 package com.android.myres.ui.order;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
 
 import androidx.databinding.DataBindingUtil;
 
@@ -14,6 +15,8 @@ import com.android.myres.databinding.ActivityOrderTrackingBinding;
 public class OrderTrackingActivity extends BaseActivity {
 
     private ActivityOrderTrackingBinding binding;
+    private CountDownTimer countDownTimer;
+    private long timeLeftInMillis = 30 * 60 * 1000; // 30 minutes
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,5 +24,35 @@ public class OrderTrackingActivity extends BaseActivity {
         binding = DataBindingUtil.setContentView(this, R.layout.activity_order_tracking);
         setActionBar(binding.toolbar);
         setActionBarTitle("Order Tracking");
+
+        binding.orderStatus.setText("Order Status: In Progress");
+        binding.orderExpectedTime.setText("Expected Time: 30 minutes");
+
+        startTimer();
+    }
+
+    private void startTimer() {
+        binding.progressBar.setMax((int) timeLeftInMillis);
+        countDownTimer = new CountDownTimer(timeLeftInMillis, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                timeLeftInMillis = millisUntilFinished;
+                updateTimer();
+            }
+
+            @Override
+            public void onFinish() {
+                binding.orderStatus.setText("Order Status: Ready for Pickup");
+            }
+        }.start();
+    }
+
+    private void updateTimer() {
+        int minutes = (int) (timeLeftInMillis / 1000) / 60;
+        int seconds = (int) (timeLeftInMillis / 1000) % 60;
+
+        String timeLeftFormatted = String.format("%02d:%02d", minutes, seconds);
+        binding.orderExpectedTime.setText("Expected Time: " + timeLeftFormatted);
+        binding.progressBar.setProgress((int) timeLeftInMillis);
     }
 }
